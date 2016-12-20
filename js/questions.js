922397343ab54dd987906cb3bfc462aa57aa80b1
IgorYarutkin <igor.yarutkin@gmail.com>
2016-12-20 21:23:09 +0300

    Исправить косяк с кавычками в разметке

--- js/questions.js
+++ js/questions.js
@@ -3,7 +3,7 @@ import renderlabelIcon from './label-answers';
 const renderGameOption = (option, questionNumber) => {
   return `
         <div class="game__option">
-          <img src=${option.src} alt=${option.alt} width=${option.width} height=${option.height}>
+          <img src="${option.src}" alt="${option.alt}" width="${option.width}" height="${option.height}">
             ${renderlabelIcon('photo', questionNumber + 1)}
             ${renderlabelIcon('paint', questionNumber + 1)}
         </div>`;
@@ -12,7 +12,7 @@ const renderGameOption = (option, questionNumber) => {
 const renderGameOptionTriple = (option) => {
   return `
         <div class="game__option ${option.selected ? 'game__option--selected' : ''}">
-          <img src=${option.src} alt=${option.alt} width=${option.width} height=${option.height}>
+          <img src="${option.src}" alt="${option.alt}" width="${option.width}" height="${option.height}">
         </div>`;
 };
 
